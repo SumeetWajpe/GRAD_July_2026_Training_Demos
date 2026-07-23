18c609d428032b74c65c46eb966075841c44b33e
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-07-23 11:22:07 +0530

    ✨ Javabasics -  Datatypes | variables

---
+++ JAVA/DAY1/UsingJavaBasics/src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //     byte branchId = 12;
+
+        // short employeeId = 2500;
+
+        // int accountNumber = 125487;
+
+        // long transactionId = 987654321456L;
+
+        // float interestRate = 6.5f;
+
+        // double accountBalance = 2456789.45;
+
+        // char accountCategory = 'P';
+
+        // boolean kycCompleted = true;
+
+        // Type casting
+        // int amount = 5000;
+        // double total = amount;
+        // double interest = 2345.88;
+        // int rounded = (int) interest;
+        
+    }
+}
