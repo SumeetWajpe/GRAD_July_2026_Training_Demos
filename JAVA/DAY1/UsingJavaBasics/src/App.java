ed86130b1683776f05022a01b3603308bd6a4660
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-07-23 11:35:59 +0530

    ✨ Using arithmetic Operators

--- JAVA/DAY1/UsingJavaBasics/src/App.java
+++ JAVA/DAY1/UsingJavaBasics/src/App.java
@@ -22,5 +22,15 @@ public class App {
         // double interest = 2345.88;
         // int rounded = (int) interest;
         
+
+        // Show Maturity Amount to user for a FD
+
+        double principle = 1000000;
+        double rate = 7.5;
+        int years = 2;
+
+        double interest = (principle* rate * years) / 100;
+        double maturity = principle + interest;
+        System.out.println("Interest -> " + interest );
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GRAD_July_2026_Training_Demos
+
+https://github.com/SumeetWajpe/GRAD_July_2026_Training_Demos
